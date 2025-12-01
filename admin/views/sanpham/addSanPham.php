e5707eda141ab95751603705fb661c8fe9ed8ed9
anhnt-ph36206-fpt <anhntph36206@fpt.edu.vn>
2025-12-02 01:33:38 +0700

    Update code danh mục sản phẩm video 3.2 để làm Bình luận sản phẩm video 6.3

--- admin/views/sanpham/addSanPham.php
+++ admin/views/sanpham/addSanPham.php
@@ -30,7 +30,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Quản lý thêm danh sách sản phẩm</h1>
+                        <h1>Quản lý Thêm danh sách sản phẩm</h1>
                     </div>
                 </div>
             </div>
