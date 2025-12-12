2b4ba3e0d03886c641a43a1cf439ec7a06f2f4ce
Thế Anh <anhntph36206@fpt.edu.vn>
2025-12-12 21:43:23 +0700

    Cập nhật báo cáo

--- admin/views/sanpham/listSanPham.php
+++ admin/views/sanpham/listSanPham.php
@@ -30,7 +30,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Quản Lý Danh Sách Thú Cưng</h1>
+            <h1>Quản Lý Danh Sách Sản Phẩm</h1>
           </div>
           <div class="col-sm-6">
             <!-- Nút thêm danh mục (tuỳ bạn) -->
